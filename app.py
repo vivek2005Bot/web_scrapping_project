a5e3e203e443832ad22c6df571a2acc98ba42882
Vivek kumar <kvivek19738@gmail.com>
2024-10-04 00:59:41 +0530

    firstcommit

---
+++ app.py
@@ -0,0 +1,105 @@
+from flask import Flask, render_template, request,jsonify
+from flask_cors import CORS,cross_origin
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as uReq
+import logging
+logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET'])
+def homepage():
+    return render_template("index.html")
+
+@app.route("/review" , methods = ['POST' , 'GET'])
+def index():
+    if request.method == 'POST':
+        try:
+            searchString = request.form['content'].replace(" ","")
+            flipkart_url = "https://www.flipkart.com/search?q=" + searchString
+            HEADERS=({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
+            uClient =requests.get(flipkart_url,headers=HEADERS)
+            flipkartPage = uClient.text
+            flipkart_html = bs(flipkartPage, "html.parser")
+            bigboxes = flipkart_html.findAll("div", {"class": "cPHDOP col-12-12"})
+            del bigboxes[0:3]
+            box = bigboxes[0]
+            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+            prodRes = requests.get(productLink,headers=HEADERS)
+            prodRes.encoding='utf-8'
+            prod_html = bs(prodRes.text, "html.parser")
+            print(prod_html)
+            commentboxes = prod_html.find_all('div', {'class': "RcXBOT"})
+
+            filename = searchString + ".csv"
+            fw = open(filename, "w",encoding='utf-8')
+            headers = "Product, Customer Name, Rating, Heading, Comment \n"
+            fw.write(headers)
+            reviews = []
+            for commentbox in commentboxes:
+                try:
+                    #name.encode(encoding='utf-8')
+                    name = commentbox.div.div.find_all('p', {'class': '_2NsDsF AwS1CA'})[0].text
+
+                except:
+                    logging.info("name")
+
+                try:
+                    #rating.encode(encoding='utf-8')
+                    rating = commentbox.div.div.div.div.text
+
+
+                except:
+                    rating = 'No Rating'
+                    logging.info("rating")
+
+                try:
+                    #commentHead.encode(encoding='utf-8')
+                    commentHead = commentbox.div.div.div.p.text
+
+                except:
+                    commentHead = 'No Comment Heading'
+                    logging.info(commentHead)
+                try:
+                    comtag = commentbox.div.div.find_all('div', {'class': ''})
+                    #custComment.encode(encoding='utf-8')
+                    custComment = comtag[0].div.text
+                except Exception as e:
+                    logging.info(e)
+
+                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
+                          "Comment": custComment}
+                reviews.append(mydict)
+
+                fw.write(f"{searchString}, {name}, {rating}, {commentHead}, {custComment}\n")
+            fw.close()
+            logging.info("log my final result {}".format(reviews))
+            uri = "mongodb+srv://Vivekkumar:M2QbrxTpSC9K#-q@cluster0.rdtbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            # Create a new client and connect to the server
+            client = MongoClient(uri, server_api=ServerApi('1'))
+            # Send a ping to confirm a successful connection
+            try:
+                 client.admin.command('ping')
+                 msg="Pinged your deployment. You successfully connected to MongoDB!"
+                 logging.info(msg)
+            except Exception as e:
+                 logging.info(e)
+            
+            db=client['review_scrap']
+            review_col_data=db['review_scrap_data']
+            review_col_data.insert_many(reviews)
+            return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
+        except Exception as e:
+            logging.info(e)
+            return 'something is wrong'
+    # return render_template('results.html')
+
+    else:
+        return render_template('index.html')
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
