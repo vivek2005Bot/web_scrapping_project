b32c6b0b0fd792a1bc677b721ba86fce5bd799ae
Vivek kumar <150792201+vivek2005Bot@users.noreply.github.com>
2024-10-06 13:44:27 +0530

    Update app.py
    Removed Cross origin

--- app.py
+++ app.py
@@ -11,12 +11,10 @@ from pymongo.server_api import ServerApi
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
-@cross_origin
 def homepage():
     return render_template("index.html")
 
 @app.route("/review" , methods = ['POST' , 'GET'])
-@cross_origin
 def index():
     if request.method == 'POST':
         try:
